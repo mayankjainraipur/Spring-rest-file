ff2f44a45065f6ff1acc8a0495c28cf55337a275
athorat <athorat@mocana.com>
2019-10-10 12:21:36 +0530

    constants

---
+++ src/main/java/com/fileopr/operations/controller/ApiConstant.java
@@ -0,0 +1,11 @@
+package com.fileopr.operations.controller;
+
+public class ApiConstant {
+    public ApiConstant() {
+    }
+    public String ADD_URL = "/add";
+    public String GET_ALL_URL = "/getAll";
+    public String DELETE_URL = "/delete/{id}";
+    public String ADD_FORM_URL = "/addByForm";
+    public String GET_BY_ID = "/get/emp/{id}";
+}
