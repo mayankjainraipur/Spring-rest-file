80931e19cb399e980fdff4cc8f883a0bbcb0c29c
athorat <athorat@mocana.com>
2019-10-10 13:22:16 +0530

    made static final

--- src/main/java/com/fileopr/operations/controller/ApiConstant.java
+++ src/main/java/com/fileopr/operations/controller/ApiConstant.java
@@ -3,9 +3,9 @@ package com.fileopr.operations.controller;
 public class ApiConstant {
     public ApiConstant() {
     }
-    public String ADD_URL = "/add";
-    public String GET_ALL_URL = "/getAll";
-    public String DELETE_URL = "/delete/{id}";
-    public String ADD_FORM_URL = "/addByForm";
-    public String GET_BY_ID = "/get/emp/{id}";
+    public static final String ADD_URL = "/add";
+    public static final String GET_ALL_URL = "/getAll";
+    public static final String DELETE_URL = "/delete/{id}";
+    public static final String ADD_FORM_URL = "/addByForm";
+    public static final String GET_BY_ID = "/get/emp/{id}";
 }
